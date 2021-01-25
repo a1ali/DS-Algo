aa889cbbf3dbd70069f5ab1d25558e55838e0b44
Afaq Ali <afaqali2010@gmail.com>
2021-01-24 23:43:33 -0500

    ch5

--- ch5_sequences/caesarCipher.py
+++ ch5_sequences/caesarCipher.py
@@ -5,3 +5,35 @@ class CaesarCipher:
         '''construct caesar cipher using given integer shift for rotation'''
         encoder = [None]*26 #temp array for encryption
         decoder = [None]*26 #temp array for decryption 
+        for k in range(26):
+            encoder[k] = chr((k + shift)%26 + ord('A'))
+            decoder[k] = chr((k - shift)%26 + ord('A'))
+        
+        self._forward = ''.join(encoder)
+        self._backward = ''.join(decoder)
+
+    def encrypt(self, message):
+        '''return string representing encritped message.'''
+        return self._transform(message, self._forward)
+
+    def decrypt(self, secret):
+        '''return decrypted message given encrypted secret.'''
+        return self._transform(secret, self._backward)
+    
+    def _transform(self, original, code):
+        '''utility to perform transformation based on given code string.'''
+        msg = list(original)
+        for k in range(len(msg)):
+            if msg[k].isupper():
+                j = ord(msg[k]) - ord('A') #index from 0 to 25
+                msg[k] = code[j] #replace this character
+        return ''.join(msg)
+
+    
+if __name__ == '__main__':
+    cipher = CaesarCipher(7)
+    message = "THE EAGLE IS IN PLAY; MEET AT JOE'S."
+    coded = cipher.encrypt(message)
+    print( 'Secret:' , coded)
+    answer = cipher.decrypt(coded)
+    print( 'Message:' , answer)
